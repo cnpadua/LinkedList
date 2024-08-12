c953c814bc4500bf2650dc6a86b18096efac9ff1
Camille <49352065+cnpadua@users.noreply.github.com>
2024-08-12 15:07:44 -0700

    Wrote isTail() and isHead() methods

--- linkedList.py
+++ linkedList.py
@@ -3,3 +3,13 @@ print("hello there")
 class Node:
 
     def __init__(self):
+        self.next = None
+        self.previous = None
+
+    def isTail(self) -> bool:
+        ''' Returns true if current node is LinkedList tail '''
+        return self.next == None
+    
+    def isHead(self) -> bool:
+        ''' Returns true if current node is LinkedList head '''
+        return self.previous == None
